cdcda6574e4cecf6d0722e02445f5c4f7596635e
David Matthews <david.matthews.1@uvm.edu>
2019-05-22 08:43:38 -0400

    incrementing version number for release 0.9

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(name='Parallel Computation',
       author='David Matthews',
-      version='0.9dev',
+      version='0.9',
       packages=['parallelpy'],
       description='Tools to support parallel computation of generic work via either MPI or a process pool',
       install_requires=['numpy', 'scipy', ]
